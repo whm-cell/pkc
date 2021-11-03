e0c7e0a3a5a5becd86b614adbe9dda6838c514ba
whm_ <fighting282367079@gmail.com>
2021-11-03 17:58:22 +0800

    重构ui

--- dense/src/main/java/com/fx/dense/controller/HomeController.java
+++ dense/src/main/java/com/fx/dense/controller/HomeController.java
@@ -1,7 +1,14 @@
 package com.fx.dense.controller;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +21,68 @@ import java.util.ResourceBundle;
  **/
 public class HomeController implements Initializable {
 
+
+    /**
+     * 动态tab页
+     */
+    @FXML
+    TabPane dynamicTabId;
+
+    @FXML
+    MenuItem aes;
+
+    @FXML
+    MenuItem des;
+
+
+
+    public void goDes(ActionEvent event){
+
+        try{
+
+            URL url = this.getClass().getClassLoader().getResource("dense.fxml");
+
+            Parent blah = FXMLLoader.load(url);
+
+            Tab tab = new Tab("Des",blah);
+
+            dynamicTabId.getTabs().add(tab);
+
+            if (event != null) {
+                dynamicTabId.getSelectionModel().select(tab);
+            }
+            tab.setOnCloseRequest((Event event1) -> {
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+
+
+
+    }
+
+    public void goAes(ActionEvent event){
+        try{
+
+            URL url = this.getClass().getClassLoader().getResource("Aes.fxml");
+
+            Parent blah = FXMLLoader.load(url);
+
+            Tab tab = new Tab("Aes",blah);
+
+            dynamicTabId.getTabs().add(tab);
+
+            if (event != null) {
+                dynamicTabId.getSelectionModel().select(tab);
+            }
+            tab.setOnCloseRequest((Event event1) -> {
+            });
+        }catch (Exception e){
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
 
 
     public void  about(ActionEvent event){
---
+++ dense/src/main/java/com/fx/dense/controller/TabMainController.java
@@ -0,0 +1,22 @@
+package com.fx.dense.controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @program: pkc
+ * @description: 主tab页
+ * @author: WangHaiMing
+ * @create: 2021-11-03 13:57
+ **/
+public class TabMainController implements Initializable {
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
