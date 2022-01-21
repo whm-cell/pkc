16b991636deb264a645802897faf7dcb9fae8221
whm_ <fighting282367079@gmail.com>
2022-01-21 17:14:11 +0800

    11

---
+++ pxx/src/main/java/com/w/h/pxx/PxxApplication.java
@@ -0,0 +1,13 @@
+package com.w.h.pxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PxxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PxxApplication.class, args);
+    }
+
+}
---
+++ pxx/src/test/java/com/w/h/pxx/PxxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w.h.pxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PxxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
